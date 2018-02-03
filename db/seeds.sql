c37987a751ad69edcf15e8e20d9fd7e43a83c762
Cwander83 <chriswandermail@gmail.com>
2018-02-02 21:27:48 -0500

    2-2-18(build table on mysql)

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+USE burgers_db;
+
+/* Insert 3 Rows into your new table */
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Bacon Cheese Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheese Burger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac burger", false);
+
